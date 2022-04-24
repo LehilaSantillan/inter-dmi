88fd43380903a0d3438b82e508c066b8af6e2757
Lehila <lehilasantillan2e39@gmail.com>
2022-04-23 21:07:50 -0500

    Refactor: Acomodo en las variables para lograr el contexto global

--- src/context/global/global.context.jsx
+++ src/context/global/global.context.jsx
@@ -1,12 +1,40 @@
-import react, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import { list } from "../../services/Posts";
+import { Auth } from "aws-amplify";
 
-const initialState = {
-    user: null,
-}
+const initialAppState = {
+  showForm: false,
+  imageURI: "",
+  user: null,
+  colorDefault: "#581845",
+};
 
-export const GlobalContext = createContext(initialState)
-export const GlobalProvider =({children}) => {
-    const [state, setState] =useState(initialState);
+export const GlobalContext = createContext(initialAppState);
+export const GlobalProvider = ({ children }) => {
+  const [state, setState] = useState(initialAppState);
+  const [post, setPosts] = useState();
+  const [usuario, setUsuario] = useState(Auth.user.username);
 
-    
-}
+  function login(email, pwd) {
+    setState((current) => ({ ...current, user: usuario }));
+  }
+
+  async function logout() {
+    try {
+      await Auth.signOut({ global: true });
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  }
+
+  async function listPosts() {
+    const postFetched = await list();
+    if (postFetched) setPosts(postFetched);
+  }
+
+    return(
+        <GlobalContext.Provider value={{ state, login, logout, listPosts, post, usuario}}>
+            {children}
+        </GlobalContext.Provider>
+    );
+};
